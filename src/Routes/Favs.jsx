c54da8353335f9e03bdeb5e44e84807da149161b
ignacioaj131982 <ignacioarevalo@gmail.com>
2024-12-02 12:47:17 -0300

    Componente favs actualizado funcionando

--- src/Routes/Favs.jsx
+++ src/Routes/Favs.jsx
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../Components/Card";
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Favs = () => {
+  const [favorites, setFavorites] = useState([]);
+
+  // Recuperamos los favoritos desde el localStorage cuando el componente se monta
+  useEffect (() => {
+    const storedFavorites = JSON.parse(localStorage.getItem('favorites')) || [];
+    setFavorites(storedFavorites);
+  }, []);
 
   return (
-    <>
-      <h1>Dentists Favs</h1>
+    <main className="main">
+      <h1>Dentistas Favoritos</h1>
       <div className="card-grid">
-        {/* este componente debe consumir los destacados del localStorage */}
-        {/* Deberan renderizar una Card por cada uno de ellos */}
+        {favorites.length > 0 ? ( 
+          favorites.map(dentist => ( 
+            <Card key={dentist.id} dentist={dentist} addToFavorites={()=> {}} />
+          ))
+        ) : (
+          <p>No tienes dentistas favoritos.</p>
+        )}
       </div>
-    </>
+    </main>
   );
 };
 
